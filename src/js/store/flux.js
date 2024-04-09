0ff63a74b749c0950fecd9d54107d44fa437e392
Javier Seiglie <96433186+jseiglie@users.noreply.github.com>
2024-04-09 14:34:40 +0000

    doc: añadidos comentarios

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -3,10 +3,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			todos: null,
 			user: null
+			todos: null,
+			user: null
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-
 			getTodosPromesas: () => {
 				fetch("https://playground.4geeks.com/todo/users/Aelf86")
 					.then(resp => resp.json())
@@ -25,13 +26,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 				try {
 					const resp = await fetch("https://playground.4geeks.com/todo/users/seiglie", opt)
-					if (resp.status == 200) {
-						console.log("se creo el usuario ---> ", data)
-						return getActions().getTodos()
+					//verificamos si respuesta diferente a 200
+					if (resp.status != 200) {
+						console.log("error creando usuario")
 					}
 					const data = await resp.json()
-					console.log("data de usuario nuevo ---> ", data);
-
+					console.log("se creo el usuario ---> ", data)
+					//se crea el usuario y se llama la funcion para traerse los todos
+					return getActions().getTodos()
 				} catch (error) {
 					console.log("error ---> ", error)
 				}
@@ -39,6 +41,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			getTodos: async () => {
 				try {
 					const resp = await fetch("https://playground.4geeks.com/todo/users/seiglie")
+					//verificamos que exista el usuario, si no existe, se llama la funcion createUser para crearlo
 					if (resp.status == 404) {
 						return getActions().createUser()
 					};
@@ -51,6 +54,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			addTask: async (task) => {
 				if (task.trim().length > 3) {
+					//creamos variable auxiliar para trabajar con el todos del store
 					const aux = getStore().todos
 					aux.push({ label: task.trim(), is_done: false })
 					setStore({ todos: aux })
@@ -60,43 +64,52 @@ const getState = ({ getStore, getActions, setStore }) => {
 							headers: {
 								"Content-Type": "application/json",
 							},
+							//al API solo se le envía la tarea a añadir, NO la lista entera
 							body: JSON.stringify({ label: task.trim(), is_done: false })
 						}
 						const resp = await fetch("https://playground.4geeks.com/todo/todos/seiglie", opt)
 						const data = await resp.json()
-						console.log(data)
+						console.log("new task added ---> ", data)
+						//no necesitamos traernos las tareas de nuevo
 						return true
 					} catch (error) {
 						console.log("error ---> ", error);
 						return false
 					}
 				}
+				//si tarea nueva es menor a 3 en largo, se lanza alerta
 				alert("Task shouln't be less than 4 characters")
 				return false
 			},
-			updateTask: async (el, i) => { 
+			updateTask: async el => {
+				if (task.trim().length > 3) {
 				try {
-					console.log(getStore().todos[i], el)
-
 					const opt = {
 						method: "PUT",
 						headers: {
 							"Content-Type": "application/json",
 						},
+						// se envia la tarea a modificar solamente
 						body: JSON.stringify(el)
 					}
-					const resp = await fetch("https://playground.4geeks.com/todo/todos/"+el.id, opt)
+					//se pasa el id de la tarea que se va a modificar en el endpoint
+					const resp = await fetch("https://playground.4geeks.com/todo/todos/" + el.id, opt)
 					const data = await resp.json()
-					console.log("updated task ", await data)
+					console.log("updated task --> ", await data)
 					return true
 				} catch (error) {
 					console.log("error ---> ", error);
 					return false
 				}
+			}
+				//si nuevo valor de la tarea es menor a 3 en largo, se lanza alerta
+			alert("Task shouln't be less than 4 characters")
+				return false
 			},
 			deleteTask: async (id) => {
 				try {
-					const aux = getStore().todos.filter(el=> el.id != id)					
+					//guardamos en un variable auxiliar la lista de tareas sin la tarea que se elimina y la guardamos en el store
+					const aux = getStore().todos.filter(el => el.id != id)
 					setStore({ todos: aux })
 					const opt = {
 						method: "DELETE",
@@ -104,15 +117,16 @@ const getState = ({ getStore, getActions, setStore }) => {
 							"Content-Type": "application/json",
 						},
 					}
-					const resp = await fetch("https://playground.4geeks.com/todo/todos/"+id, opt)
+					//se envía el id de la tarea a eliminar
+					const resp = await fetch("https://playground.4geeks.com/todo/todos/" + id, opt)
 					const data = await resp.json()
-					console.log("deleted task ", await data)
+					console.log("deleted task ---> ", await data)
 					return true
 				} catch (error) {
 					console.log("error ---> ", error);
 					return false
 				}
-			 }
+			}
 
 
 		}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 import { Context } from "../store/appContext";
 
 export const Home = () => {
@@ -13,37 +15,40 @@ export const Home = () => {
 		// actions.getTodosPromesas()
 	}, [])
 
-	const handleDelete = (el) => { console.log(el) }
-
 	const handleSubmit = e => {
 		e.preventDefault()
 		actions.addTask(task)
 	}
 
-	const handleEdit = (e, el, i, method) => {
+	const handleEdit = (e, el, method) => {
 		e.preventDefault()
 		console.log(el, method)
-		method == "put" ? actions.updateTask(el, i) : actions.deleteTask(el.id)
+		//método como prop dice cual función se va a ejecutar
+		method == "put" ? actions.updateTask(el) : actions.deleteTask(el.id)
 	}
 
 	return (
 		<div className="text-center mt-5">
 			<div className="container">
 				<form className="form-control bg-secondary text-white" onSubmit={e => handleSubmit(e)}>
+					{/* el usuario de la lista de tareas */}
 					<p className="text-end my-0 ">user: @{store.user && store.user}</p>
 					<h1>Todos</h1>
 					<input type="text" className="form-control my-3" onChange={e => setTask(e.target.value)} />
 					<input type="submit" hidden value={"Add"} className="form-control" />
 
 					{store.todos?.length > 0 ?
+					// solo se puede devolver un hijo, por eso utilizamos un fragmento <></>
 						(<>
-							{store.todos?.map((el, i) =>
-								<div key={i} className="input-group flex-nowrap d-flex mt-1">
+							{store.todos?.map(el =>
+							// la clase "input-group" nos permite agrupar en el input más elementos html
+								<div key={el.id} className="input-group flex-nowrap d-flex mt-1">
 									<input type="text" className="form-control border-0" defaultValue={el.label} onChange={(e) => el.label = e.target.value} />
-									<button className="btn btn-light " onClick={e => handleEdit(e, el, i, "put")} ><span className="fa-regular fa-pen-to-square text-success"></span></button>
-									<button className="btn btn-light " onClick={e => handleEdit(e, el, i, "delete")}><span className="fa-regular fa-trash-can text-danger"></span></button>
+									<button className="btn btn-light " onClick={e => handleEdit(e, el, "put")} ><span className="fa-regular fa-pen-to-square text-success"></span></button>
+									<button className="btn btn-light " onClick={e => handleEdit(e, el, "delete")}><span className="fa-regular fa-trash-can text-danger"></span></button>
 								</div>
 							)}
+							{/* contador de tareas */}
 							<p className="text-start mt-3">Tasks count: {store.todos?.length}</p>
 						</>
 						)
